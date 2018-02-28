c18f4df2f129f5c9d143c84e0657745e752757c8
Michael <materekhov.sbt@sberbank.ru>
2018-02-28 17:53:21 +0300

    reseting the application type

---
+++ sources/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  macwad
+//
+//  Created by Michael on 28.02.2018.
+//  Copyright © 2018 Michael. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+	@autoreleasepool {
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	}
+	return 0;
+}
